6a2367ebb3c2d0058c2e3a45e38d917c2c933ae5
Ayman Alharbi <Ayman.f.alharbi@gmail.com>
2025-04-15 14:43:21 +0300

    Second Commit

---
+++ src/main/java/com/example/barbershopmanagementsystem/Api/ApiException.java
@@ -0,0 +1,7 @@
+package com.example.barbershopmanagementsystem.Api;
+
+public class ApiException extends RuntimeException{
+    public ApiException(String message){
+        super(message);
+    }
+}
